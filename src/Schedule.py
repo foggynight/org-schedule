42c978d8f48cdd3b241ab89cf82ebdf9ead3bc78
foggynight <foggynight709@gmail.com>
2020-10-24 04:04:27 -0230

    Update class Schedule
    Schedule.py
    - Update constructor comment
    - Parse file during initialization
    
    Parsing is done by splitting a file on lines containing only 'NEXT'.

--- src/Schedule.py
+++ src/Schedule.py
@@ -3,10 +3,21 @@ class Schedule:
     Schedule entry generator for emacs org-mode.
     """
     def __init__(self, schedule_path):
+        """ @params
+        - schedule_path {string}: Path to the schedule file
+        """
         file = open(schedule_path)
         lines = file.readlines()
 
-        data = []
+        data = [[]]
+        ptr = 0
+        for line in [l.rstrip('\n') for l in lines]:
+            if line == 'NEXT':
+                data.append([])
+                ptr += 1
+            else:
+                data[ptr].append(line)
+
         self.week = {'M':'', 'T':'', 'W':'', 'R':'', 'F':''}
 
         file.close()
