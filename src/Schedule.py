e8e3aebd0ff4ce87c834f35f5e58c4c0f0084ea9
foggynight <foggynight709@gmail.com>
2020-10-24 19:04:22 -0230

    Add method parse_schedule
    Schedule.py
    - Remove member week
    - Add method parse_schedule: Parse schedule file
    - Add member data: Result of parsing schedule file
    
    parse_schedule splits a file on lines containing only '---'.

--- src/Schedule.py
+++ src/Schedule.py
@@ -6,18 +6,29 @@ class Schedule:
         """ @params
         - schedule_path {string}: Path to the schedule file
         """
+        self.data = self.parse_schedule(schedule_path)
+
+    def parse_schedule(self, schedule_path):
+        """
+        Parse the schedule file.
+
+        - Days are separated by lines containing only '---'
+
+        @params
+        - schedule_path {string}: Path to the schedule file
+        """
         file = open(schedule_path)
         lines = file.readlines()
 
         data = [[]]
-        ptr = 0
-        for line in [l.rstrip('\n') for l in lines]:
-            if line == 'NEXT':
-                data.append([])
-                ptr += 1
-            else:
-                data[ptr].append(line)
+        index = 0
 
-        self.week = {'M':'', 'T':'', 'W':'', 'R':'', 'F':''}
+        for line in [l.rstrip('\n') for l in lines]:
+            if line == '---':
+                data.append([])
+                index += 1
+            else:
+                data[index].append(line)
 
         file.close()
+        return data
